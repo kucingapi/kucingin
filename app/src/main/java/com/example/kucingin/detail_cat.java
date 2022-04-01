a7e161f723d9034bbbe035bb30a0c3f87ff18b3d
Novel Bafagih <novel.bafagih@udana.id>
2022-04-01 14:12:28 +0700

    [ADD] detail cat using binding

--- app/src/main/java/com/example/kucingin/detail_cat.java
+++ app/src/main/java/com/example/kucingin/detail_cat.java
@@ -2,12 +2,14 @@ package com.example.kucingin;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.kucingin.databinding.ActivityDetailCatBinding;
 
 public class detail_cat extends AppCompatActivity {
-
+    private ActivityDetailCatBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_cat);
+        binding = ActivityDetailCatBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
