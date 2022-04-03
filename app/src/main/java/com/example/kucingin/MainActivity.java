bef78201e86e22cf0561b07279b4e3fa7567eec4
Novel Bafagih <novel.bafagih@udana.id>
2022-04-03 12:39:12 +0700

    [FIX] action bar change

--- app/src/main/java/com/example/kucingin/MainActivity.java
+++ app/src/main/java/com/example/kucingin/MainActivity.java
@@ -27,7 +27,6 @@ private ActivityMainBinding binding;
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
