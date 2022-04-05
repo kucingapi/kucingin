48a4d860781ede47bd5d391ad904864b8eaf1feb
Novel Bafagih <novel.bafagih@udana.id>
2022-04-05 13:44:11 +0700

    [ADD] adding username to catfood

--- app/src/main/java/com/example/kucingin/ui/catFood/CatFoodFragment.java
+++ app/src/main/java/com/example/kucingin/ui/catFood/CatFoodFragment.java
@@ -1,9 +1,11 @@
 package com.example.kucingin.ui.catFood;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -18,11 +20,18 @@ public class CatFoodFragment extends Fragment {
     private FragmentCatFoodBinding binding;
     private RecyclerView popular;
     private Card[] dataset;
+    private Intent intent;
+    private TextView username;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentCatFoodBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        intent = getActivity().getIntent();
+        username = binding.usernameTextview;
+
+        username.setText(intent.getStringExtra("username"));
 
         initDataset();
         CardAdapter customAdapter = new CardAdapter(dataset);
